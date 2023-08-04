2e3022ca782861d97ae5c4a16da7917a13bedb0a
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-08-04 23:36:09 +0600

    routes added

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,12 +1,24 @@
 
+
 const router = require('express').Router();
 const db = require('../db/db');
 
+// For ticketId
 router
-.route('tickets/t/:ticketId')
+.route('/t/:ticketId')
 .get(()=>{})
 .patch(()=>{})
 .delete(()=>{})
+
+router.get('/u/:username',()=>{});
+router.patch('/u/:username',()=>{});
+router.delete('/u/:username',()=>{});
+
+router.post('/sell',()=>{});
+router.post('/bulk',()=>{});
+router.get('/draw',()=>{})
+router.get('/',()=>{})
+
 
 // const router = require('express').Router();
 // const db = require('../db/db');
